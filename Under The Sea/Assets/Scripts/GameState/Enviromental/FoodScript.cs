98afc509c560b4e1e60931d1709c81a3a8534a95
Mick Holst <mick.holst@gmail.com>
2014-04-07 22:33:10 +0200

    Added food, with behaviour script and Instatiate script (Game is playable again).

---
+++ Under The Sea/Assets/Scripts/GameState/Enviromental/FoodScript.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FoodScript : MonoBehaviour
+{
+    public float Lifetime;
+    public float MinRandomFactor;
+    public float MaxRandomFactor;
+    public float FoodValue;
+    public float Speed;
+
+    private bool shallISink;
+
+    private GameObject Particles;
+
+    private Transform thisTransform;
+
+    void OnEnable()
+    {
+        shallISink = true;
+        thisTransform = transform;
+        //SetFoodProps();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        var bubbles = transform.FindChild("Bubbles");
+        Particles = bubbles.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shallISink == true)
+            thisTransform.Translate(Vector3.down * Speed * Time.deltaTime);
+    }
+
+    /*
+    void SetFoodProps()
+    {
+        float RandomFactor = Random.Range(MinRandomFactor, MaxRandomFactor);
+        transform.localScale = new Vector3(transform.localScale.x * RandomFactor, transform.localScale.y * RandomFactor, transform.localScale.z * RandomFactor);
+        FoodValue = 1 * RandomFactor;
+        curSpeed = Speed * SpeedManipulationFactor;
+        curSpeed = curSpeed * RandomFactor;
+    }
+     * */
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "FloorTrigger")
+        {
+            shallISink = false;
+            if (Particles != null)
+            {
+                Particles.GetComponent<ParticleSystem>().loop = false;
+                Particles.GetComponent<ParticleSystem>().emissionRate = 0;
+            }
+            StartCoroutine(DelayDeath());
+        }
+        
+
+    }
+
+    IEnumerator DelayDeath()
+    {
+        yield return new WaitForSeconds(Lifetime);
+        Destroy(gameObject);
+    }
+}
--- Under The Sea/Assets/Scripts/GameState/Player/InstantiateFood.cs
+++ Under The Sea/Assets/Scripts/GameState/Player/InstantiateFood.cs
@@ -3,19 +3,28 @@ using System.Collections;
 
 public class InstantiateFood : MonoBehaviour {
 
+    public GameObject FoodPrefab;
 
+    // Use this for initialization
+    void Start()
+    {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    }
 
-    void InstantiatenFood(GameObject FoodPrefab) { 
-        
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SpawnFood(FoodPrefab);
+        }
+    }
+
+    void SpawnFood(GameObject SpawningFood)
+    {
+            var mousePos = Input.mousePosition;
+            mousePos.z = 5f;
+            var objectPos = Camera.main.ScreenToWorldPoint(mousePos);
+            GameObject clone = Instantiate(SpawningFood, objectPos, Quaternion.identity) as GameObject;
     }
 }
