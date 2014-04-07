4168ff477c9284cdb064583dd12325572c503ac6
Mick Holst <mick.holst@gmail.com>
2014-04-08 00:06:47 +0200

    Food is now only spawnable in the "active aquarium", pretty neat.

--- Under The Sea/Assets/Scripts/GameState/Player/InstantiateFood.cs
+++ Under The Sea/Assets/Scripts/GameState/Player/InstantiateFood.cs
@@ -4,11 +4,20 @@ using System.Collections;
 public class InstantiateFood : MonoBehaviour {
 
     public GameObject FoodPrefab;
+    public float OffsetX;
+    public float OffsetY;
+
+    private GameObject activeAquarium;
+
+    private float minX;
+    private float maxX;
+    private float minY;
+    private float maxY;
 
     // Use this for initialization
     void Start()
     {
-
+        ChangeSpawnCords();
     }
 
     // Update is called once per frame
@@ -17,6 +26,10 @@ public class InstantiateFood : MonoBehaviour {
         if (Input.GetMouseButtonDown(0))
         {
             SpawnFood(FoodPrefab);
+            Debug.Log("min X " + minX);
+            Debug.Log("max X " + maxX);
+            Debug.Log("min Y " + minY);
+            Debug.Log("max Y " + maxY);
         }
     }
 
@@ -25,6 +38,18 @@ public class InstantiateFood : MonoBehaviour {
             var mousePos = Input.mousePosition;
             mousePos.z = 5f;
             var objectPos = Camera.main.ScreenToWorldPoint(mousePos);
-            GameObject clone = Instantiate(SpawningFood, objectPos, Quaternion.identity) as GameObject;
+
+
+            GameObject clone = Instantiate(SpawningFood, new Vector3(Mathf.Clamp(objectPos.x, minX, maxX), Mathf.Clamp(objectPos.y, minY, maxY), objectPos.z), Quaternion.identity) as GameObject;
+    }
+
+    public void ChangeSpawnCords() {
+        activeAquarium = GameObject.FindGameObjectWithTag("GameHandler").GetComponent<GameHandler>().ActiveAquarium;
+        Transform aquaCord = activeAquarium.transform;
+
+        minX = (aquaCord.position.x - (aquaCord.localScale.x / 2)) + OffsetX;
+        maxX = (aquaCord.position.x + (aquaCord.localScale.x / 2)) - OffsetX;
+        minY = (aquaCord.position.y - (aquaCord.localScale.y / 2)) + OffsetY;
+        maxY = (aquaCord.position.y + (aquaCord.localScale.y / 2)) - OffsetY;
     }
 }
